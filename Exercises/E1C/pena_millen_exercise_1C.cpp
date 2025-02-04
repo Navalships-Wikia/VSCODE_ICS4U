11881e09bd12a831be1f932deb8554ea70a7ffe7
NavalShips-Wikia <millpen55@gmail.com>
2025-02-04 14:12:28 -0500

    day 3

---
+++ Exercises/E1C/pena_millen_exercise_1C.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void mice(){
+    cout<<"Three blind mice\n";
+}
+
+void run(){
+    cout<<"See how they run\n";
+}
+
+int main(){
+    mice();
+    mice();
+    run();
+    run();
+
+cout<<endl;
+return 0;
+}

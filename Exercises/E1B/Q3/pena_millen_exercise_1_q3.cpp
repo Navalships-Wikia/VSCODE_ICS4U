1a343bf035472296007868091611a95e4886a541
NavalShips-Wikia <millpen55@gmail.com>
2025-02-06 10:24:45 -0500

    day 5

--- Exercises/E1B/Q3/pena_millen_exercise_1_q3.cpp
+++ Exercises/E1B/Q3/pena_millen_exercise_1_q3.cpp
@@ -8,7 +8,7 @@ int main(){
 
     cout<<"Enter a sentence: ";
     getline(cin, sentence);
-    for (int i=; i<sentence.size()-2, i++;){
+    for (int i=0; i<sentence.size()-2; i++){
         if (sentence[i]=='f' && sentence[i+1]=='o' && sentence[i+2]=='o')
         sentence[i]='x';
         sentence[i+1]='x';

5cba4d8b168f93f3b5f2b0571d4bbf114eb84c51
NavalShips-Wikia <millpen55@gmail.com>
2025-03-07 09:20:21 -0500

    Day 17

--- Exercises/E3/pena_millen_e3_q10/pena_millen_e3_q10.cpp
+++ Exercises/E3/pena_millen_e3_q10/pena_millen_e3_q10.cpp
@@ -3,51 +3,115 @@ using namespace std;
 
 // Function declaration
 void insertion_sort(int a[], int size);
+int search(int a[], int size, int item);
 
 int main()
 {
+    cout<<endl;
+    
     // Ask user for input
     int size;
-    cout << "Enter the size of your Array: ";
-    cin >> size;
+    cout<<"Enter the size of your Array: ";
+    cin>>size;
 
-    int a[size];
-    cout << "Enter a list of numbers to be sorted: ";
-    for (int i = 0; i < size; i++) {
-        cin >> a[i];
+    int array[size];
+    cout<<"Enter a list of numbers to be sorted: ";
+    for (int i=0; i<size; i++) {
+        cin>>array[i];
     }
 
     // Call sort function
-    insertion_sort(a, size);
+    insertion_sort(array, size);
 
     // Output of sorted array in descending order
     cout<<"Sorted array: ";
     for (int i=0; i<size; i++)
     {
-        cout<<a[i]<<" ";
+        cout<<array[i]<<" ";
     }
+    cout<<endl;
+
+    // Ask user whether if they want to delete a number from the array
+    char choice;
+    cout<<"\nDo you want to delete a number from the array? (Y/N): ";
+    cin>>choice;
+
+    if (choice=='Y' || choice=='y')
+    {
+        int num;
+        int index;
+       
+        // Constantly prompt user to enter a nummber until it is valid
+        do {
+            cout<<"Enter the number you want to delete: ";
+            cin>>num;
+    
+            // Call search function to find the number in the array
+            index=search(array, size, num);
+
+            // prompt to try again
+            if (index==-1)
+            {
+                cout<<"Number not found in the array, Please try again."<<endl;
+            }
+        } while (index==-1);
+
+        // delete chosen number
+        for (int i=search(array, size, num); i<size; i++)
+        {
+            array[i]=array[i+1];
+        }
+        size--;
+    }
+    // If user says no, then exit.
+    else if (choice=='N' || choice=='n')
+    {
+        cout<<"No number will be deleted, Goodbye"<<endl<<endl;
+        return 0;
+    }
+
+    cout<<endl;
+
+    // Output of changed array
+    cout<<"Updated array: ";
+    for (int i=0; i<size; i++)
+    {
+        cout<<array[i]<<" ";
+    }
+    cout<<endl; 
 
     cout<<endl;
     return 0;
 }
 
 // Functions
-void insertion_sort(int b[], int size)
+void insertion_sort(int a[], int size)
 {
     int top;
     int item;
     int i;
     for (top=1; top<size; top++)
     {
-        item=b[top];
+        item=a[top];
         i=top;
 
-        while(i>0 &&item>b[i-1])
+        while(i>0 &&item>a[i-1])
         {
-            b[i]=b[i-1];
+            a[i]=a[i-1];
             i--;
         }
-
-        b[i]=item;
+        a[i]=item;
     }
+}
+
+int search(int a[], int size, int item)
+{
+    for (int i=0; i<size; i++)
+    {
+        if (a[i]==item)
+        {
+            return i;
+        }
+    }
+    return -1;
 }
